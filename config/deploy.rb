367435d2496fd56bfd7fdf3cabbc6b04b385197c
pump <patrawut.pump@gmail.com>
2017-07-24 03:42:55 +0700

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,8 +4,16 @@ lock "3.8.2"
 set :deploy_to, '/home/deploy/tickerly-web'
 set :repo_url, "git@github.com:PushZone/tickerly.git"
 
+set :ssh_options, { forward_agent: true }
+set :application, 'tickerly-web' #change this to the name of your app
+set :deploy_to, '/home/deploy/tickerly-web'
+set :use_sudo, true
+set :branch, 'develop' #or whichever branch you want to use
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+
+
 append :linked_files, "config/database.yml", "config/secrets.yml"
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
+# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,9 +1,3 @@
+set :stage, :production
 server '128.199.155.238', user: 'deploy', roles: %w{web app db}
 
-set :ssh_options, { forward_agent: true }
-set :application, 'tickerly-web' #change this to the name of your app
-set :repo_url, 'git@github.com:abovelab/tickerly-web.git' #change this to the url of your app on github
-set :deploy_to, '/home/deploy/tickerly-web'
-set :use_sudo, true
-set :branch, 'develop' #or whichever branch you want to use
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
